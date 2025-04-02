30c4e179654935c060b2dd6821c6c670cde5c481
frahz <me@frahz.dev>
2025-04-01 22:39:01 -0700

    feat: nix gui package and macos support

--- flake.nix
+++ flake.nix
@@ -12,7 +12,9 @@
   }: let
     systems = [
       "x86_64-linux"
+      "x86_64-darwin"
       "aarch64-linux"
+      "aarch64-darwin"
     ];
     forEachSystem = nixpkgs.lib.genAttrs systems;
     pkgsForEach = forEachSystem (system:
@@ -23,11 +25,12 @@
   in {
     devShells = forEachSystem (system: let
       pkgs = pkgsForEach.${system};
-      libPath = with pkgs; lib.makeLibraryPath [
-        libGL
-        libxkbcommon
-        wayland
-      ];
+      libPath = with pkgs;
+        lib.makeLibraryPath [
+          libGL
+          libxkbcommon
+          wayland
+        ];
     in {
       default = pkgs.mkShell {
         buildInputs = [
@@ -40,26 +43,54 @@
     });
     packages = forEachSystem (system: let
       pkgs = pkgsForEach.${system};
-      cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-cli/Cargo.toml);
     in {
-      mdrop = pkgs.rustPlatform.buildRustPackage {
-        inherit (cargoToml.package) name version;
+      mdrop = let
+        cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-cli/Cargo.toml);
+      in
+        pkgs.rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
 
-        src = ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
-        cargoFlags = [
-          "--bin"
-          "mdrop-cli"
-        ];
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          cargoFlags = [
+            "--bin"
+            "mdrop"
+          ];
 
-        meta = with pkgs.lib; {
-          description = "Linux CLI tool for controlling Moondrop USB audio dongles.";
-          homepage = "https://github.com/frahz/mdrop";
-          license = licenses.mit;
+          meta = with pkgs.lib; {
+            description = "Linux CLI tool for controlling Moondrop USB audio dongles.";
+            homepage = "https://github.com/frahz/mdrop";
+            license = licenses.mit;
+          };
         };
-      };
+      gui = let
+        cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-gui/Cargo.toml);
+      in
+        pkgs.rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
+
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "cryoglyph-0.1.0" = "sha256-8QCTD5OLO05RmwaugblYbKPPN7hfckctQlaBydPNDPE=";
+              "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+              "iced-0.14.0-dev" = "sha256-YC74qowoW9VJonluX/FuiQc+TvvBytskhvgCLpmknQg=";
+            };
+          };
+          cargoFlags = [
+            "--bin"
+            "mdrop-gui"
+          ];
+
+          meta = with pkgs.lib; {
+            description = "Linux CLI tool for controlling Moondrop USB audio dongles.";
+            homepage = "https://github.com/frahz/mdrop";
+            license = licenses.mit;
+          };
+        };
       default = self.packages.${system}.mdrop;
     });
   };
