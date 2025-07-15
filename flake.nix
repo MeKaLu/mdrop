f7a40a37ad5334580dd6ad8364553abfcae00dd0
frahz <me@frahz.dev>
2025-07-14 21:58:56 -0700

    chore: format nix files

--- flake.nix
+++ flake.nix
@@ -4,106 +4,120 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    ...
-  }: let
-    systems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-    forEachSystem = nixpkgs.lib.genAttrs systems;
-    pkgsForEach = forEachSystem (system:
-      import nixpkgs {
-        inherit system;
-        overlays = [rust-overlay.overlays.default];
-      });
-  in {
-    devShells = forEachSystem (system: let
-      pkgs = pkgsForEach.${system};
-      libPath = with pkgs;
-        lib.makeLibraryPath [
-          libGL
-          libxkbcommon
-          wayland
-        ];
-    in {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rust-bin.stable.latest.default
-          pkgs.alejandra
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          function (
+            import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          libPath =
+            with pkgs;
+            lib.makeLibraryPath [
+              libGL
+              libxkbcommon
+              wayland
+            ];
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.rust-bin.stable.latest.default
+              pkgs.alejandra
+            ];
 
-        LD_LIBRARY_PATH = libPath;
-      };
-    });
-    packages = forEachSystem (system: let
-      pkgs = pkgsForEach.${system};
-    in {
-      mdrop = let
-        cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-cli/Cargo.toml);
-      in
-        pkgs.rustPlatform.buildRustPackage {
-          inherit (cargoToml.package) name version;
-
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+            LD_LIBRARY_PATH = libPath;
           };
-          cargoFlags = [
-            "--bin"
-            "mdrop"
-          ];
-
+        }
+      );
+      packages = forAllSystems (
+        pkgs:
+        let
           meta = with pkgs.lib; {
             description = "Linux CLI tool for controlling Moondrop USB audio dongles.";
             homepage = "https://github.com/frahz/mdrop";
             license = licenses.mit;
           };
-        };
-      gui = let
-        cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-gui/Cargo.toml);
-        libPath = with pkgs;
-          lib.makeLibraryPath [
-            libGL
-            libxkbcommon
-            wayland
-          ];
-      in
-        pkgs.rustPlatform.buildRustPackage {
-          inherit (cargoToml.package) name version;
 
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            outputHashes = {
-              "cryoglyph-0.1.0" = "sha256-8QCTD5OLO05RmwaugblYbKPPN7hfckctQlaBydPNDPE=";
-              "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
-              "iced-0.14.0-dev" = "sha256-YC74qowoW9VJonluX/FuiQc+TvvBytskhvgCLpmknQg=";
+        in
+        rec {
+          mdrop =
+            let
+              cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-cli/Cargo.toml);
+            in
+            pkgs.rustPlatform.buildRustPackage {
+              inherit (cargoToml.package) name version;
+              inherit meta;
+
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
+              cargoFlags = [
+                "--bin"
+                "mdrop"
+              ];
             };
-          };
-          cargoFlags = [
-            "--bin"
-            "mdrop-gui"
-          ];
+          gui =
+            let
+              cargoToml = builtins.fromTOML (builtins.readFile ./mdrop-gui/Cargo.toml);
+              libPath =
+                with pkgs;
+                lib.makeLibraryPath [
+                  libGL
+                  libxkbcommon
+                  wayland
+                ];
+            in
+            pkgs.rustPlatform.buildRustPackage {
+              inherit (cargoToml.package) name version;
+              inherit meta;
 
-          nativeBuildInputs = [pkgs.makeWrapper];
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "cryoglyph-0.1.0" = "sha256-8QCTD5OLO05RmwaugblYbKPPN7hfckctQlaBydPNDPE=";
+                  "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+                  "iced-0.14.0-dev" = "sha256-YC74qowoW9VJonluX/FuiQc+TvvBytskhvgCLpmknQg=";
+                };
+              };
+              cargoFlags = [
+                "--bin"
+                "mdrop-gui"
+              ];
 
-          postInstall = ''
-            wrapProgram $out/bin/mdrop-gui --prefix LD_LIBRARY_PATH : ${libPath}
-          '';
+              nativeBuildInputs = [ pkgs.makeWrapper ];
 
-          meta = with pkgs.lib; {
-            description = "Linux CLI tool for controlling Moondrop USB audio dongles.";
-            homepage = "https://github.com/frahz/mdrop";
-            license = licenses.mit;
-          };
-        };
-      default = self.packages.${system}.mdrop;
-    });
-  };
+              postInstall = ''
+                wrapProgram $out/bin/mdrop-gui --prefix LD_LIBRARY_PATH : ${libPath}
+              '';
+            };
+          default = mdrop;
+        }
+      );
+    };
 }
